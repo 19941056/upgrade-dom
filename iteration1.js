524cf624586ca7bc1c2d89e168c3464e7fa346d7
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MacBook-Pro-de-Antonio.local>
2021-10-12 19:33:26 +0200

    iteration1

---
+++ iteration1.js
@@ -0,0 +1,17 @@
+//1.1
+const showme= document.querySelector(".showme");
+console.log(showme);
+
+//1.2
+const pillado=document.querySelector("#pillado");
+console.log(pillado)
+
+//1.3
+const parrafo=document.querySelectorAll('p');
+console.log(parrafo)
+//1.4
+const pokemon=document.querySelectorAll('.pokemon');
+console.log(pokemon)
+//1.5
+const dataFunction=document.querySelector["data-function='testMe'"];
+console.log(dataFunction);
